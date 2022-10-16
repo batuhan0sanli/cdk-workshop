077df846f4f892a8af53b95ef371574652f06816
Batuhan Şanlı <batuhansanli@gmail.com>
2022-10-16 22:29:08 +0300

    add hello_js lambda function

--- lib/aws-cdk-learning-stack.ts
+++ lib/aws-cdk-learning-stack.ts
@@ -1,8 +1,16 @@
 import { Stack, StackProps } from 'aws-cdk-lib';
+import * as lambda from 'aws-cdk-lib/aws-lambda'
 import { Construct } from 'constructs';
 
 export class AwsCdkLearningStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
+
+    // defines an AWS Lambda resource
+    const hello = new lambda.Function(this, 'HelloHandlerJs', {
+      runtime: lambda.Runtime.NODEJS_14_X,
+      code: lambda.Code.fromAsset('lambda'),
+      handler: 'hello_js.Handler'
+    })
   }
 }
